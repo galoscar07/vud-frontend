8161813cdd9381b343f8baa145ab5cfdd847c46a
Gal Oscar <galoscar05@gmail.com>
2023-01-13 20:56:31 +0200

    Added project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Route, Routes} from "react-router-dom";
+
+// Pages
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+// Footer
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Homepage /> } />
+        <Route path="login" element={ <Login /> } />
+        <Route path="register" element={ <Register /> } />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <div className={'button'}>
+        Click me
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
