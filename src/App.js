16aa35a840cb9ba544c6101bd0291f9aaf41eebc
Ioana Constantinescu <constantinescu.ioana09@yahoo.com>
2023-01-16 21:18:16 +0200

    add profile page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Register from "./pages/Register";
 // Footer
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AddProfile from "./pages/AddProfile";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={ <Homepage /> } />
         <Route path="login" element={ <Login /> } />
         <Route path="register" element={ <Register /> } />
+        <Route path="add-profile" element={ <AddProfile /> } />
       </Routes>
       <Footer />
     </div>
