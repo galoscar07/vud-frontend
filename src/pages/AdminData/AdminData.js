420cb26f0071ce97de0aa0a7819190491e2e97e2
Ioana Constantinescu <constantinescu.ioana09@yahoo.com>
2023-01-21 20:56:28 +0200

    grid

--- src/pages/AdminData/AdminData.js
+++ src/pages/AdminData/AdminData.js
@@ -37,53 +37,80 @@ const AdminData = () => {
                     <div className="contact-data">
                         <div className="container-title">Date de contact</div>
                         <div className="fields-wrapper">
-                            <div className="input-wrapper">
-                                <label>*Nume</label>
-                                <input name="lastName" type="text" required value={values.lastName}
-                                       onChange={(e) => {
-                                           handleFieldChange(e.target.value, e.target.name);
-                                       }}/>
+                            <div className="col-2">
+                                <div className="input-wrapper">
+                                    <label>*Nume</label>
+                                    <input name="lastName" type="text" required value={values.lastName}
+                                           onChange={(e) => {
+                                               handleFieldChange(e.target.value, e.target.name);
+                                           }}/>
+                                </div>
+                                <div className="input-wrapper">
+                                    <label>*Prenume</label>
+                                    <input name="firstName" type="text" required value={values.firstName}
+                                           onChange={(e) => {
+                                               handleFieldChange(e.target.value, e.target.name);
+                                           }}/>
+                                </div>
                             </div>
-                            <div className="input-wrapper">
-                                <label>*Prenume</label>
-                                <input name="firstName" type="text" required value={values.firstName}
-                                       onChange={(e) => {
-                                           handleFieldChange(e.target.value, e.target.name);
-                                       }}/>
-                            </div>
-                            <div className="input-wrapper">
-                                <label>Telefon</label>
-                                <input name="phoneNo" type="text" value={values.phoneNo}
-                                       onChange={(e) => {
-                                           handleFieldChange(e.target.value, e.target.name);
-                                       }}/>
-                            </div>
-                            <div className="input-wrapper">
-                                <label>Email de contact</label>
-                                <input name="email" type="text" value={values.email}
-                                       onChange={(e) => {
-                                           handleFieldChange(e.target.value, e.target.name);
-                                       }}/>
+                            <div className="col-2">
+                                <div className="input-wrapper">
+                                    <label>Telefon</label>
+                                    <input name="phoneNo" type="text" value={values.phoneNo}
+                                           onChange={(e) => {
+                                               handleFieldChange(e.target.value, e.target.name);
+                                           }}/>
+                                </div>
+                                <div className="input-wrapper">
+                                    <label>Email de contact</label>
+                                    <input name="email" type="text" value={values.email}
+                                           onChange={(e) => {
+                                               handleFieldChange(e.target.value, e.target.name);
+                                           }}/>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="company-data">
                         <div className="container-title">Date de companie</div>
                         <div className="fields-wrapper">
+                            <div className="col-2">
+                                <div className="input-wrapper">
+                                    <label>Functia</label>
+                                    <input name="position" type="text" value={values.position}
+                                           onChange={(e) => {
+                                               handleFieldChange(e.target.value, e.target.name);
+                                           }}/>
+                                </div>
+                                <div className="input-wrapper">
+                                    <label>Compania</label>
+                                    <input name="company" type="text" value={values.company}
+                                           onChange={(e) => {
+                                               handleFieldChange(e.target.value, e.target.name);
+                                           }}/>
+                                </div>
+                            </div>
+                            <div className="col-3">
                             <div className="input-wrapper">
-                                <label>Functia</label>
-                                <input name="position" type="text" value={values.position}
-                                       onChange={(e) => {
-                                           handleFieldChange(e.target.value, e.target.name);
-                                       }}/>
+                                <label>*Oras</label>
+                                <select required id="city" name="city">
+                                    <option value="Alba-Iulia">Alba-Iulia</option>
+                                    <option value="Cluj-Napoca">Cluj-Napoca</option>
+                                    <option value="Bucuresti">Bucuresti</option>
+                                    <option value="Sibiu">Sibiu</option>
+                                </select>
                             </div>
                             <div className="input-wrapper">
-                                <label>Compania</label>
-                                <input name="company" type="text" value={values.company}
-                                       onChange={(e) => {
-                                           handleFieldChange(e.target.value, e.target.name);
-                                       }}/>
+                                <label>Judet</label>
+                                <select id="county" name="county">
+                                    <option value="Alba">Alba</option>
+                                    <option value="Cluj">Cluj</option>
+                                    <option value="Bucuresti">Bucuresti</option>
+                                    <option value="Sibiu">Sibiu</option>
+                                </select>
                             </div>
+                        </div>
+                        <div className="col-2">
                             <div className="input-wrapper">
                                 <label>*Strada</label>
                                 <input name="street" type="text" required value={values.street}
@@ -100,26 +127,27 @@ const AdminData = () => {
                             </div>
                         </div>
                     </div>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page
-                        when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                        distribution of letters, as opposed to using 'Content here, content here', making it look like
-                        readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
-                        their default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-                        their infancy. </p>
-                    <div className="image-upload">
-                        <label htmlFor="file">
-                            <img
-                                src="images/upload.svg"/>
-                        </label>
-                        <input style={{display: "none"}} name="files" id="file" type="file" multiple
-                               onChange={(e) => updateList(e.target.value, e.target.name)}/>
-                        <div id="file-list"></div>
-                    </div>
-                    <input className="button" type="submit" value="Mergi mai departe"/>
-                </form>
             </div>
-        </div>
-    )
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page
+                when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution of letters, as opposed to using 'Content here, content here', making it look like
+                readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
+                their default model text, and a search for 'lorem ipsum' will uncover many web sites still in
+                their infancy. </p>
+            <div className="image-upload">
+                <label htmlFor="file">
+                    <img
+                        src="images/upload.svg"/>
+                </label>
+                <input style={{display: "none"}} name="files" id="file" type="file" multiple
+                       onChange={(e) => updateList(e.target.value, e.target.name)}/>
+                <div id="file-list"></div>
+            </div>
+            <input className="button" type="submit" value="Mergi mai departe"/>
+        </form>
+</div>
+</div>
+)
 }
 
 
