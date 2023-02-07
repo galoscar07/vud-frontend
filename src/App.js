cd9f3684fb8201878f2526ae488e2ef575d19842
Ioana Constantinescu <constantinescu.ioana09@yahoo.com>
2023-02-07 15:21:00 +0200

    ????

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.scss';
 import {Route, Routes} from "react-router-dom";
 
 // Pages
-import Homepage from "./pages/Homepage";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Homepage from "./pages/Homepage/Homepage";
+import Login from "./pages/Auth/Login/Login";
+import Register from "./pages/Auth/Register/Register";
 
 // Footer
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AddProfile from "./pages/AddProfile";
 import ForgetPassword from "./pages/ForgetPassword";
+import AdminData from "./pages/AdminData";
 
 function App() {
   return (
