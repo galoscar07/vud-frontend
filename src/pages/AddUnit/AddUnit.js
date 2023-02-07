40bf3cf05674bbec6ef5f10197b6f96ba16a40c8
Ioana Constantinescu <constantinescu.ioana09@yahoo.com>
2023-02-07 14:59:54 +0200

    ????

---
+++ src/pages/AddUnit/AddUnit.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./AddUnit.scss"
+
+const AddUnit = () => {
+
+    return (
+        <div className="add-unit-page">
+            <img src="/public/images/unit.svg"/>
+            <h1>Adaugă Unitate medicală</h1>
+           {/*<Dropdown>*/}
+        {/* here */}
+        <h1>AIIIICICIIC</h1>
+        </div>
+    );
+}
+
+export default AddUnit;
