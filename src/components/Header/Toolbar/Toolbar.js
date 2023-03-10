c0f7bd69c621bd1fa72df0a4115e846e584ee33a
Ioana Constantinescu <constantinescu.ioana09@yahoo.com>
2023-03-10 15:13:08 +0200

    wip add active link;

--- src/components/Header/Toolbar/Toolbar.js
+++ src/components/Header/Toolbar/Toolbar.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Toolbar.scss'
 import { NavLink, useNavigate } from "react-router-dom";
 import { routes } from "../../../utils/routes";
 
+const toolbarValues = [
+  {
+    text: "Administrare Date Profil",
+    value: "profile-data"
+  },
+  {
+    text: "Date administrator",
+    value: "admin-data"
+  },
+  {
+    text: "Administrare Unitate Medicala",
+    value: "unit-data"
+  },
+]
+
 function Toolbar() {
+  let url = window.location.toString()
+  let currentlyActive = url.split("dashboard/")[1];
+  console.log(currentlyActive, 'CURR')
+  const [activeLink, setActive] = React.useState(currentlyActive);
+  console.log(url, 'INIT')
+  useEffect(() => {
+    console.log(url, 'aiii')
+    // let url = window.location.toString()
+    // let currentlyActive = url.split("dashboard/")[1];
+    // console.log(currentlyActive, 'CURR')
+    // const [activeLink, setActive] = React.useState(currentlyActive);
+    setActive(url.split("dashboard/")[1]);
+    console.log(activeLink, 'ACTIVE')
+  }, [url])
 
   return (
     <div className={'toolbar'}>
       <div className={'toolbar-items'}>
-        <NavLink
-          to={routes.DASHBOARD_PROFILE_DATA}
-        >Administrare Date Profil</NavLink>
-        <NavLink
-          to={routes.DASHBOARD_ADMIN_DATA}
-        >Date administrator</NavLink>
-        <NavLink
-          to={routes.DASHBOARD_UNIT_DATA}
-        >Administrare Unitate Medicala</NavLink>
-
+        {toolbarValues.map((el, i) =>
+          <NavLink key={i} className={window.location.toString().includes(el.value) ? 'active' : 'inactive'}
+            to={routes.DASHBOARD + "/" + el.value}>{el.text}</NavLink>
+        )}
       </div>
     </div>
   );
