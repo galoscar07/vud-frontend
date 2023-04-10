cd4eb9f9b08cecbbeec7576c122efa27dd5366b7
Ioana Constantinescu <constantinescu.ioana09@yahoo.com>
2023-04-10 14:53:03 +0300

    banner gifs

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,6 @@
 // const API_URL_DEV = 'http://127.0.0.1:8000/'
 const API_URL = 'http://vud-api.eu-central-1.elasticbeanstalk.com/'
-export const API_URL_MEDIA = 'http://vud-api.eu-central-1.elasticbeanstalk.com/'
+export const API_URL_MEDIA = ''
 
 export const API_MAP = {
   // Creates new account and send link to verify email
